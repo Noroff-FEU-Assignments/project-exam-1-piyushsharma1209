d930a738424544b4ac17eb8e97a700a18a0bed10
piyu010 <piyush.sharma@stud.noroff.no>
2022-10-02 23:14:19 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Making Function
+function ready() {
+}
+
+
+
+// navbar for mobile
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+  navbar.classList.toggle('active');
+}
